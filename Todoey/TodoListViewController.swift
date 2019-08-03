88b57e6350d6d1b632a0242909f4182412ff7d18
Evamay Dela Rosa <evamay_dict@evamays-macbook.local>
2019-08-03 20:20:48 +0800

    Reload if item is added

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
 
-    let itemArray = ["Find Vaughn", "Vanne", "love you Daddy"]
+    var itemArray = ["Find Vaughn", "Vanne", "love you Daddy"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -39,5 +39,31 @@ class TodoListViewController: UITableViewController {
         }
             tableView.deselectRow(at: indexPath, animated: true)
     }
+    
+    //MARK - Add New Items
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add new todoey item", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add item", style: .default) { (action) in
+            
+            //what will happen once the user click the add button
+            
+            self.itemArray.append(textField.text!)
+            self.tableView.reloadData()
+            
+        }
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create new item"
+            print(alertTextField.text)
+            textField = alertTextField
+        }
+        
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
 }
 
